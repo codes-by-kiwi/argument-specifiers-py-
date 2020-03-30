6a657b7a4de88705a76012797d233670d0014215
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-30 14:30:47 +0200

    Add files via upload

---
+++ argspe.py
@@ -0,0 +1,8 @@
+# This prints out "John is 23 years old."
+name = "Mary"
+name2 = "Bob"
+age = 1
+print("%s had a little lamb named %s who was %d years old." % (name, name2, age)) #three arg specifiers here --> name, name2 and age 
+
+#multiple argument specifiers in the last line so 
+#you use a tuple or brackets within which you write them 
